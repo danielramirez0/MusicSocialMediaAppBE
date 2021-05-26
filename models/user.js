d32aee7cdef1787c5f0d1d2c6e84d8ba63245058
Daniel Ramirez <notused@notused.net>
2021-05-25 17:47:29 -0700

    Adds email verification to Joi validation for user

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { Friend } = require("./friend");
+const { Post } = require("./post");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, require: true, minlength: 6, maxlength: 50 },
+  password: { type: String, require: true, minlength: 3, maxlength: 20 },
+  firstName: { type: String, require: true, minlength: 2, maxlength: 50 },
+  lastName: { type: String, require: true, minlength: 2, maxlength: 50 },
+  favoriteArtist: { type: String },
+  favoriteAlbum: { type: String },
+  favoriteSong: { type: String },
+  friends: [Friend.Schema],
+  posts: [Post.Schema],
+  pendingFriends: [Friend.Schema],
+});
+
+const user = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    email: Joi.string().min(6).max(50).required().email(),
+    password: Joi.string().min(3).max(20).required(),
+    firstName: Joi.string().min(2).max(50).required(),
+    lastName: Joi.string().min(2).max(50).required(),
+    favoriteArtist: Joi.string(),
+    favoriteAlbum: Joi.string(),
+    favoriteSong: Joi.string(),
+    friends: [],
+    posts: [],
+    pendingFriends: [],
+  });
+  return schema.validate(user);
+}
+
+exports.User = user;
+exports.validateUser = validateUser;
+exports.userSchema = userSchema;
